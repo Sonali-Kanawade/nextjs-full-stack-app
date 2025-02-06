7a42533089b44781f16cf08d750bdd117939488e
Sonali Kanawade <sonalikanawade111@gmail.com>
2025-02-06 16:33:21 +0530

    basic file structure

---
+++ src/lib/dbConnect.ts
@@ -0,0 +1,27 @@
+import mongoose from "mongoose";
+
+
+type ConnectionObject = {
+    isConnected?: number
+}
+
+const connection: ConnectionObject = {};
+
+async function dbConect(): Promise<void> {
+    if(connection.isConnected){
+        console.log("already connected to database");
+        return;
+    }
+
+    try {
+        const db = await mongoose.connect(process.env.MONGODB_URI || "")
+        connection.isConnected = db.connections[0].readyState
+        console.log("DB connected successfully.")
+    } catch (error) {
+        console.log("DB connection failed", error)
+        process.exit(1)
+    }
+}
+
+
+export default dbConect;
